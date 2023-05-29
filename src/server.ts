10576d6598daeb228910c562cd90c97543179a5d
Your Name Main <you@example.com>
2023-05-28 23:30:28 -0700

    Testing

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config/index"
-
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
 async function bootstrap() {
-try {
-
-    await mongoose.connect(config.database_url as string);
-    console.log('Database connected successfully');
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected successfully')
 
     app.listen(config.port, () => {
-        console.log(`Application app listening on port ${config.port}`)
-      })
-
-} catch (error) {
-
-    console.log('Failed to database connected',error);
-
-}
-
-
+      console.log(`Application app listening on port ${config.port}`)
+    })
+  } catch (error) {
+    console.log('Failed to database connected', error)
+  }
 }
 
 bootstrap()
